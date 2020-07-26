555438dd8418c07db12dd77f953f95bbfec5c942
jruthe <jordan.ruthe@gmail.com>
2020-07-26 08:21:28 -0400

    Bugfix and added all devices to status if no device is selected

--- moonraker/plugins/power.py
+++ moonraker/plugins/power.py
@@ -1,5 +1,10 @@
 # Printer power manipulations
 #
+#   GET  /printer/power/devices             # List devices
+#   GET  /printer/power/status?dev=devname  # Get device status
+#   POST /printer/power/on?dev=devname      # Turn device on
+#   POST /printer/power/off?dev=devname     # Turn device off
+#
 # Add to printer.cfg:
 #   [moonraker_plugin power]
 #   devices: printer, led           (comma separated list of devices)
@@ -11,19 +16,18 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 #
-#   Sample printer_on script for pi:
-#     #!/bin/bash
-#     gpio export 23 out
-#     gpio -g write 23 0
-#     sleep 1
-#     sudo service klipper restart
-#
-#   Sample printer_on script:
+#   Sample on script for pi:
 #     #!/bin/bash
 #     gpio export 23 out
 #     gpio -g write 23 1
 #
+#   Sample off script:
+#     #!/bin/bash
+#     gpio export 23 out
+#     gpio -g write 23 0
 #
+#
+#   Sample status script:
 #     #!/bin/bash
 #     status=$(cat /sys/class/gpio/gpiochip0/subsystem/gpio23/value)
 #     if [ $status -eq 0 ]
@@ -41,10 +45,17 @@
 import logging
 
 class PrinterPower:
+    paths = {
+        "status":   "/printer/power/status",
+        "on":       "/printer/power/on",
+        "off":      "/printer/power/off"
+    }
+
+
     def __init__(self, server):
         self.server = server
         self.server.register_endpoint(
-            "/printer/power/devices", "power_status", ['GET'],
+            "/printer/power/devices", "power_devices", ['GET'],
             self._handle_list_devices)
         self.server.register_endpoint(
             "/printer/power/status", "power_status", ['GET'],
@@ -56,51 +67,66 @@ class PrinterPower:
             "/printer/power/off", "power_off", ['POST'],
             self._handle_machine_request)
         
-        self.printer_status = "unknown"
-        self.cmds = {}
+        self.current_dev = None
+        self.devices = {}
     
     async def _handle_list_devices(self, path, method, args):
         output = {"devices": []}
-        for dev in self.cmds:
-            output['devices'].append(dev)
+        for dev in self.devices:
+            output['devices'].append({
+                "name": self.devices[dev]["name"],
+                "id": dev
+            })
         return output
 
     async def _handle_machine_request(self, path, method, args):
-        dev = args.get('dev')
-        if dev not in self.cmds:
-            return "device_not_found"
+        if len(args) == 0:
+            if path == self.paths['status']:
+                args = self.devices
+            else:
+                return "no_devices"
+         
+        result = {}
+        for dev in args:
+            if dev not in self.devices:
+                result[dev] = "device_not_found"
+                continue
+            
+            if path == self.paths['status']:
+                cmd = self.devices[dev]["cmds"]["status"]
+            elif path == self.paths['on']:
+                cmd = self.devices[dev]["cmds"]["on"]
+                action = "on"
+            elif path == self.paths['off']:
+                cmd = self.devices[dev]["cmds"]["off"]
+                action = "off"
+            else:
+                raise self.server.error("Unsupported power request")
+            
+            if cmd == None:
+                raise self.server.error("Command not configured in printer.cfg under [moonraker_plugins power] for device " + dev)
+            
+            self.current_dev = dev
+            shell_command = self.server.lookup_plugin('shell_command')
+            scmd = shell_command.build_shell_command(cmd, self.get_cmd_output)
+            try:
+                await scmd.run()
+            except Exception:
+                logging.exception("Error running cmd '%s'" % (cmd))
         
-        if path == "/printer/power/status":
-            cmd = self.cmds[dev]["status"]
-        elif path == "/printer/power/on":
-            cmd = self.cmds[dev]["on"]
-        elif path == "/printer/power/off":
-            cmd = self.cmds[dev]["off"]
-        else:
-            raise self.server.error("Unsupported power request")
-        
-        if cmd == None:
-            raise self.server.error("Command not configured in printer.cfg under [moonraker_plugins power] for device " + dev)
-        
-        shell_command = self.server.lookup_plugin('shell_command')
-        scmd = shell_command.build_shell_command(cmd, self.get_cmd_output)
-        try:
-            await scmd.run()
-        except Exception:
-            logging.exception("Error running cmd '%s'" % (cmd))
-        
-        if path == "/printer/power/status":
-            return {"power": self.printer_status}
-        else:
-            return "ok"
+            if path == self.paths['status']:
+                result[dev] = self.devices[dev]["status"]
+            else:
+                result[dev] = action
+        return result
     
     def get_cmd_output(self, partial_output):
         if "on" in str(partial_output):
-            self.printer_status = "on"
+            self.devices[self.current_dev]["status"] = "on"
         elif "off" in str(partial_output):
-            self.printer_status = "off"
+            self.devices[self.current_dev]["status"] = "off"
         else:
-            self.printer_status = "unknown"
+            self.devices[self.current_dev]["status"] = "unknown"
      
     def load_config(self, config):
         if "devices" not in config.keys():
@@ -113,12 +139,16 @@ class PrinterPower:
         
         for dev in devices:
             dev = dev.strip()
-            self.cmds[dev] = {}
-            
+            name = config[dev + "_name"] if dev + "_name" in config.keys() else dev
+            self.devices[dev] = {
+                "name": name,
+                "cmds": {}
+            }
+
             for key in keys:
                 logging.info(dev + "_" + key)
                 id = dev + "_" + key
-                self.cmds[dev][key] = config[id] if id in config.keys() else None
+                self.devices[dev]["cmds"][key] = config[id] if id in config.keys() else None
         
 
 def load_plugin(server):
