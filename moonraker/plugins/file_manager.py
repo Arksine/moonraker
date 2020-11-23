b0f781ec165ac66bd08a91ed0b2d226898cd7b33
Arksine <arksine.code@gmail.com>
2020-11-23 06:17:23 -0500

    file_manager:  serve Klipper's "docs" directory
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- moonraker/plugins/file_manager.py
+++ moonraker/plugins/file_manager.py
@@ -65,6 +65,8 @@ class FileManager:
         if klipper_path is not None:
             example_cfg_path = os.path.join(klipper_path, "config")
             self.register_directory("config_examples", example_cfg_path)
+            docs_path = os.path.join(klipper_path, "docs")
+            self.register_directory("docs", docs_path)
 
         # Register log path
         log_file = paths.get('log_file')
