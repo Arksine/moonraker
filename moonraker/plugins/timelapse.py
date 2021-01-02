9fc3dad399380e3a9951949c78ddbd71b3ed9585
FrYakaTKoP <fryakatkop@gmail.com>
2021-01-02 19:43:53 +0100

    timelapse: force pixel format yuv420
    solved problem where older media player could not playback the video (reproduced on android smartphone)
    
    Signed-off-by:  Christoph Frei <fryakatkop@gmail.com>

--- moonraker/plugins/timelapse.py
+++ moonraker/plugins/timelapse.py
@@ -1,4 +1,4 @@
-# Timelapse plugin 
+# Timelapse plugin
 #
 # Copyright (C) 2020 Christoph Frei <fryakatkop@gmail.com>
 #
@@ -11,27 +11,30 @@ import glob
 from datetime import datetime
 from tornado.ioloop import IOLoop
 
+
 class Timelapse:
-    
+
     def __init__(self, config):
         # setup global vars
         self.framecount = 0
-        self.enabled = config.getboolean("enabled" , True) 
-        self.crf = config.getint("constant_rate_factor" , 23) 
-        self.framerate = config.getint("output_framerate" , 30)
+        self.enabled = config.getboolean("enabled", True)
+        self.crf = config.getint("constant_rate_factor", 23)
+        self.framerate = config.getint("output_framerate", 30)
         self.timeformatcode = config.get("time_format_code", "%Y%m%d_%H%M")
-        self.snapshoturl = config.get("snapshoturl", "http://localhost:8080/?action=snapshot")
+        self.snapshoturl = config.get(
+            "snapshoturl", "http://localhost:8080/?action=snapshot")
         self.extraoutputparams = config.get("extraoutputparams", "")
-        out_dir_cfg = config.get("output_path" , "~/timelapse/")
+        out_dir_cfg = config.get("output_path", "~/timelapse/")
         self.out_dir = os.path.expanduser(out_dir_cfg)
         self.temp_dir = "/tmp/timelapse/"
-        
-        # setup directories 
-        os.makedirs(self.temp_dir, exist_ok=True)   
+        self.lastcmdreponse = ""
+
+        # setup directories
+        os.makedirs(self.temp_dir, exist_ok=True)
         os.makedirs(self.out_dir, exist_ok=True)
-        
+
         # setup eventhandlers and endpoints
-        self.server = config.get_server() 
+        self.server = config.get_server()
         file_manager = self.server.lookup_plugin("file_manager")
         file_manager.register_directory("timelapses", self.out_dir)
         self.server.register_event_handler(
@@ -41,13 +44,14 @@ class Timelapse:
         self.server.register_endpoint(
             "/machine/timelapse/finish", ['POST'], self.timelapse_finish)
         self.server.register_endpoint(
-            "/machine/timelapse/settings", ['GET', 'POST'], self.webrequest_timelapse_settings)
-                
+            "/machine/timelapse/settings", ['GET', 'POST'],
+            self.webrequest_timelapse_settings)
+
     async def webrequest_timelapse_settings(self, webrequest):
         action = webrequest.get_action()
         if action == 'POST':
             args = webrequest.get_args()
-            #logging.info("webreq_args: " + str(args))
+            # logging.info("webreq_args: " + str(args))
             for arg in args:
                 val = args.get(arg)
                 if arg == "enabled":
@@ -67,16 +71,16 @@ class Timelapse:
             'snapshoturl': self.snapshoturl,
             'extraoutputparams': self.extraoutputparams
             }
-  
+
     def call_timelapse_newframe(self):
         if self.enabled:
             ioloop = IOLoop.current()
             ioloop.spawn_callback(self.timelapse_newframe)
         # else:
             # logging.info("NEW_FRAME macro ignored timelapse is disabled")
-      
+
     async def timelapse_newframe(self):
-        self.framecount += 1        
+        self.framecount += 1
         framefile = "frame" + str(self.framecount).zfill(6) + ".jpg"
         cmd = "wget " + self.snapshoturl + " -O " \
               + self.temp_dir + framefile
@@ -87,12 +91,12 @@ class Timelapse:
             await scmd.run(timeout=2., verbose=False)
         except Exception:
             logging.exception(f"Error running cmd '{cmd}'")
-        
+
     async def webrequest_timelapse_finish(self, webrequest):
         ioloop = IOLoop.current()
         ioloop.spawn_callback(self.timelapse_finish)
         return "ok"
-            
+
     def handle_status_update(self, status):
         if status == "File selected":
             # print_started
@@ -102,71 +106,71 @@ class Timelapse:
             if self.enabled:
                 ioloop = IOLoop.current()
                 ioloop.spawn_callback(self.timelapse_finish)
-            
+
     def timelapse_cleanup(self):
         # logging.info("timelapse_cleanup")
         filelist = glob.glob(self.temp_dir + "frame*.jpg")
         if filelist:
             for filepath in filelist:
                 os.remove(filepath)
-        self.framecount = 0     
-        
+        self.framecount = 0
+
     async def timelapse_finish(self, webrequest=None):
         # logging.info("timelapse_finish")
         filelist = glob.glob(self.temp_dir + "frame*.jpg")
         if not filelist:
-            msg = "timelapse_finish: no frames, skip video render"
+            msg = "no frames to render skipping"
             logging.info(msg)
             status = "skipped"
-            cmd = None 
-            outfile = None
+            cmd = outfile = None
         else:
             self.framecount = 0
             klippy_apis = self.server.lookup_plugin("klippy_apis")
             result = await klippy_apis.query_objects({'print_stats': None})
             pstats = result.get("print_stats", {})
-            gcodefile = pstats.get("filename", "") #.split(".", 1)[0]
-            #logging.info(f"gcodefile: {gcodefile}")
-            now = datetime.now() 
+            gcodefile = pstats.get("filename", "")  # .split(".", 1)[0]
+            # logging.info(f"gcodefile: {gcodefile}")
+            now = datetime.now()
             date_time = now.strftime(self.timeformatcode)
             inputfiles = self.temp_dir + "frame%6d.jpg"
             outsuffix = ".mp4"
-            outfile = self.out_dir + "timelapse_" \
-                    + gcodefile + "_" + date_time + outsuffix
+            outfile = "timelapse_" + gcodefile + "_" + date_time + outsuffix
             cmd = "ffmpeg" \
                   + " -r " + str(self.framerate) \
                   + " -i '" + inputfiles + "'" \
                   + " -crf " + str(self.crf) \
                   + " -vcodec libx264" \
+                  + " -pix_fmt yuv420p" \
                   + " " + self.extraoutputparams \
-                  + " '" + outfile + "' -y" 
+                  + " '" + self.out_dir + outfile + "' -y"
             logging.info(f"start FFMPEG: {cmd}")
             shell_command = self.server.lookup_plugin("shell_command")
             scmd = shell_command.build_shell_command(cmd, self.ffmpeg_response)
             try:
-                cmdstatus = await scmd.run(timeout=None, verbose=False)
+                cmdstatus = await scmd.run(timeout=None, verbose=True)
             except Exception:
                 logging.exception(f"Error running cmd '{cmd}'")
-            
-            # check completion of the Popen process
-            # warning for now this don't represent the real statuscode of the execution!
+
+            # check success
             if cmdstatus:
-                status = "completed"
-                msg = f"Rendering Video completed: {outfile}"    
-            else:  
+                status = "success"
+                msg = f"Rendering Video successful: {outfile}"
+            else:
                 status = "error"
-                msg = f"Rendering Video failed"   
-                
+                response = self.lastcmdreponse.decode("utf-8")
+                msg = f"Rendering Video failed: {response}"
+
         return {
                 'status': status,
+                'msg': msg,
                 'file': outfile,
-                'cmd': cmd,
-                'msg': msg
+                'cmd': cmd
             }
-            
-                
-    def ffmpeg_response(self, response, statuscode):
-        logging.info(f"ffmpegResponse: {response}")
-            
+
+    def ffmpeg_response(self, response):
+        # logging.info(f"ffmpegResponse: {response}")
+        self.lastcmdreponse = response
+
+
 def load_plugin(config):
     return Timelapse(config)
