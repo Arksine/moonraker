e9dfa82a6826db57fae95ba56e48516aac71adfc
Jordan Ruthe <jordan.ruthe@gmail.com>
2021-02-28 23:38:56 -0500

    history: Integrate lmdb, save metadata

--- moonraker/plugins/history.py
+++ moonraker/plugins/history.py
@@ -4,14 +4,18 @@
 import json, logging, time
 from tornado.ioloop import IOLoop
 
+SAVE_INTERVAL = 5
+
 class History:
     def __init__(self, config):
         self.server = config.get_server()
-        self.db = self.server.lookup_plugin('database')
+        self.database = self.server.lookup_plugin("database")
+        self.gcdb = self.database.wrap_namespace("gcode_metadata",  parse_keys=False)
         self.current_job = None
         self.file_metadata = None
         self.jobs = {}
-        self.job_id = 0
+        self.job_id = -1
+        self.last_update_time = 0
         self.print_stats = {}
 
         self.server.register_event_handler(
@@ -23,16 +27,20 @@ class History:
         self.server.register_endpoint(
             "/printer/jobs/list", ['GET'], self._handle_jobs_list)
 
+        self.database.register_local_namespace("history")
+
         try:
-            jobs = self.db.get("history")
-            if jobs != None:
-                id = max([int(id) for id in list(jobs)])
-                for job in list(jobs):
-                    self.jobs[job] = PrinterJob(jobs[job])
-                    self.job_id = id + 1
+            self.job_id = self.database.get_item("history","job_auto_inc_id")
         except:
-            logging.info("Error reading history from database. Starting "
-                "empty database")
+            logging.info("Creating job history namespace in database")
+            self.database.insert_item("history","job_auto_inc_id", 0)
+            self.job_id = 0
+            self.database.insert_item("history","jobs", self.jobs)
+
+        jobs = self.database.get_item("history","jobs")
+        for job in jobs:
+            self.jobs[job] = PrinterJob(jobs[job])
+        logging.debug("Job history: %s" % self.jobs)
 
     async def _init_ready(self):
         klippy_apis = self.server.lookup_plugin('klippy_apis')
@@ -76,34 +84,61 @@ class History:
                     if new_state == "printing" and old_state != "paused":
                         for s in list(ps):
                             self.print_stats[s] = ps[s]
-                        self.current_job = PrinterJob(self.print_stats)
-                        self.add_job(self.current_job)
-                    elif new_state == "complete":
+                        self.add_job(PrinterJob(self.print_stats))
+                    elif new_state == "complete" and self.current_job != None:
                         for s in list(ps):
                             self.print_stats[s] = ps[s]
-
-                        self.current_job.finish("completed", self.print_stats)
-                        self.current_job = None
-
-                        self.save_to_database()
-
-                    elif new_state == "standby":
-                        self.current_job.finish("cancelled", self.print_stats)
+                        self.finish_job("completed", self.print_stats)
+                    elif new_state == "standby"  and self.current_job != None:
+                        self.finish_job("cancelled", self.print_stats)
 
             for s in list(ps):
                 self.print_stats[s] = ps[s]
 
+            if time.time() > self.last_update_time + SAVE_INTERVAL:
+                if self.current_job != None:
+                    self.last_update_time = time.time()
+                    self.jobs[self.current_job].update_from_ps(self.print_stats)
+                    self.save_current_job()
+
     def add_job(self, job):
         job_id = self.job_id
         self.job_id += 1
+        self.database.insert_item("history","job_auto_inc_id", self.job_id)
 
-        job.set("id", job_id)
+        self.current_job = job_id
         self.jobs[job_id] = job
+        self.save_current_job()
+        self.save_job_metadata()
+
+    def save_job_metadata(self):
+        if self.current_job == None or self.current_job not in self.jobs:
+            return
+
+        gcdb = self.database.wrap_namespace("gcode_metadata",  parse_keys=False)
+        logging.debug("gcdb: %s" % gcdb)
+        filename = self.jobs[self.current_job].get("filename")
+        if filename not in gcdb:
+            return
+
+        self.database.insert_item("history","metadata.%s" % self.current_job,
+            gcdb[filename])
+
+    def finish_job(self, status, updates):
+        if self.current_job == None:
+            return
+
+        self.jobs[self.current_job].finish("completed", self.print_stats)
+        self.save_current_job()
+        self.current_job = None
+
+    def save_current_job(self):
+        self.database.insert_item("history", "jobs.%s" %
+            self.current_job, self.jobs[self.current_job].__dict__)
 
     def save_to_database(self):
-        self.db.set("history", json.loads(json.dumps(self.jobs,
-            default=lambda o: o.__dict__)))
-        self.db.request_save()
+        self.database.insert_item("history", "jobs",
+            json.loads(json.dumps(self.jobs,default=lambda o: o.__dict__)))
 
     def get_job(self, id):
         if id not in self.jobs:
@@ -112,7 +147,6 @@ class History:
 
 class PrinterJob:
     def __init__(self, data={}, file_metadata=None):
-        self.id = 0
         self.end_time = None
         self.filament_used = 0
         self.filename = None
@@ -121,31 +155,37 @@ class PrinterJob:
         self.start_time = time.time()
         self.total_duration = 0
         self.file_metadata = file_metadata
-        self._update_data(data)
+        self.update_from_ps(data)
         self._update_file_metadata(file_metadata)
+        
     def finish(self, status, print_stats):
         self.end_time = time.time()
         self.status = status
-        self._update_data(print_stats)
+        self.update_from_ps(print_stats)
+
     def get(self, name):
         if not hasattr(self, name):
             return None
         return getattr(self, name)
+
     def get_stats(self):
         return {k: getattr(self, k) for k in self.__dict__
             if k != "file_metadata"}
     def get_metadata(self):
         return self.file_metadata
+
     def set(self, name, val):
         if not hasattr(self, name):
             return
         setattr(self, name, val)
-    def _update_file_metadata(self, file_metadata):
-        self.file_metadata = file_metadata
-    def _update_data(self, data):
+
+    def update_from_ps(self, data):
         for i in data:
             if hasattr(self, i):
                 setattr(self, i, data[i])
+
+    def _update_file_metadata(self, file_metadata):
+        self.file_metadata = file_metadata
 
 def load_plugin(config):
     return History(config)
