abfbf6a3d6b0f0499ad533459f3e1d9e74edcf3c
Jordan Ruthe <jordan.ruthe@gmail.com>
2021-03-11 10:34:18 -0500

    history: Update metadata functionality. Move auto_inc key to moonraker namespace

--- moonraker/plugins/history.py
+++ moonraker/plugins/history.py
@@ -1,13 +1,13 @@
 # History cache for printer jobs
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import json, logging
+import json, logging, time
 from tornado.ioloop import IOLoop
 
 SAVE_INTERVAL = 5
 
 HIST_NAMESPACE = "history"
-JOBS_AUTO_INC_KEY = "job_auto_inc_id"
+JOBS_AUTO_INC_KEY = "history_auto_inc_id"
 
 class History:
     def __init__(self, config):
@@ -16,8 +16,7 @@ class History:
         self.gcdb = self.database.wrap_namespace("gcode_metadata",
             parse_keys=False)
         self.current_job = None
-        self.jobs = []
-        self.job_id = -1
+        self.current_job_id = None
         self.print_stats = {}
 
         self.server.register_event_handler(
@@ -34,27 +33,13 @@ class History:
             "/server/history/delete", ['DELETE'], self._handle_job_delete)
         self.server.register_endpoint(
             "/server/history/list", ['GET'], self._handle_jobs_list)
-        self.server.register_endpoint(
-            "/server/history/metadata", ['GET'], self._handle_job_metadata)
 
         self.database.register_local_namespace(HIST_NAMESPACE)
         self.history_ns = self.database.wrap_namespace(HIST_NAMESPACE,
             parse_keys=False)
-        try:
-            cur_job_id = self.history_ns.get(JOBS_AUTO_INC_KEY)
-        except:
-            logging.info("Creating job history namespace in database")
-            self.history_ns.insert(JOBS_AUTO_INC_KEY, 0)
 
-        self.jobs = []
-        self.metadata = []
-        keys = self.history_ns.keys()
-        keys.remove(JOBS_AUTO_INC_KEY)
-        for key in keys:
-            if key.startswith("md_"):
-                self.metadata.append(key[3:])
-            else:
-                self.jobs.append(key)
+        if JOBS_AUTO_INC_KEY not in self.database.ns_keys("moonraker"):
+            self.database.insert_item("moonraker", JOBS_AUTO_INC_KEY, 0)
 
     async def _init_ready(self):
         klippy_apis = self.server.lookup_plugin('klippy_apis')
@@ -70,13 +55,11 @@ class History:
         id = str(web_request.get_int("id", -1))
         if all:
             deljobs = []
-            jobs = self.jobs.copy()
-            for job in jobs:
+            jobs = self.history_ns.keys()
+            for job in self.history_ns.keys():
                 self.delete_job(job, False)
                 deljobs.append(job)
-            for key in self.history_ns.keys():
-                self.history_ns.delete(key);
-            self.history_ns.insert(JOBS_AUTO_INC_KEY, 0);
+            self.database.update_item("moonraker", JOBS_AUTO_INC_KEY, 0);
             self.metadata = []
             return deljobs
 
@@ -92,23 +75,23 @@ class History:
     async def _handle_jobs_list(self, web_request):
         id = str(web_request.get_int("id", -1))
         if id != "-1":
-            if id not in self.history_ns.keys():
+            if id not in self.history_ns:
                 raise self.server.error(f"Invalid job id: {id}")
-            return {id: self.get_job(id).get_stats()}
+            return {id: self.history_ns.get(id, {})}
 
         before = web_request.get_float("before", -1)
         since = web_request.get_float("since", -1)
         limit = web_request.get_int("limit", 50)
         start = web_request.get_int("start", 0)
-        if start > (len(self.jobs)-1) or len(self.jobs) == 0:
-            return {"count": len(self.jobs), "prints": {}}
+        if start > (len(self.history_ns)-1) or len(self.history_ns) == 0:
+            return {"count": len(self.history_ns), "prints": {}}
 
         i = 0
-        end_num = len(self.jobs)
+        end_num = len(self.history_ns)
         jobs = {}
         start_num = 0
 
-        for id in self.jobs:
+        for id in self.history_ns.keys():
             job = self.history_ns.get(id)
             if since != -1 and since > job.get('start_time'):
                 start_num += 1
@@ -125,14 +108,6 @@ class History:
             i += 1
 
         return {"count": end_num - start_num, "prints": jobs}
-
-    async def _handle_job_metadata(self, web_request):
-        id = str(web_request.get_int("id", -1))
-        if id == "-1" or id not in self.jobs:
-            raise self.server.error(f"Invalid job id: {id}")
-
-        metadata = self.get_metadata(id)
-        return metadata
 
     async def _status_update(self, data):
         if "print_stats" in data:
@@ -159,35 +134,16 @@ class History:
             self.save_current_job()
 
     def add_job(self, job):
-        self.current_job_id = str(self.history_ns.get(JOBS_AUTO_INC_KEY))
-        self.history_ns.update_child(JOBS_AUTO_INC_KEY,
+        self.current_job_id = str(self.database.get_item("moonraker",
+            JOBS_AUTO_INC_KEY))
+        self.database.update_item("moonraker", JOBS_AUTO_INC_KEY,
             int(self.current_job_id)+1)
-        self.jobs.append(self.current_job_id)
         self.current_job = job
         self.grab_job_metadata()
         self.history_ns.insert(self.current_job_id, job.get_stats())
 
     def delete_job(self, id, check_metadata=True):
         id = str(id)
-
-        if id in self.jobs:
-            self.jobs.remove(id)
-
-        if check_metadata == True:
-            other_jobs_using_metadata = False
-            filename = self.get_job(id).get("filename")
-            index = self.get_job(id).get("metadata")
-            for job in self.jobs:
-                if self.get_job(job).get("filename") == filename:
-                    if self.get_job(job).get("metadata") == index:
-                        other_jobs_using_metadata = True
-
-            if other_jobs_using_metadata == False:
-                if len(self.history_ns.get([f"md_{filename}"]).keys()) == 1:
-                    self.metadata.remove(f"{filename}")
-                    self.history_ns.delete([f"md_{filename}"])
-                else:
-                    self.history_ns.delete([f"md_{filename}", index])
 
         if id in self.history_ns.keys():
             self.history_ns.delete(id)
@@ -211,13 +167,6 @@ class History:
             return None
         return self.history_ns.get(id)
 
-    def get_metadata(self, id):
-        job = self.get_job(id)
-        md_dict = self.history_ns.get([f"md_{job['filename']}"])
-        if job['metadata'] not in md_dict:
-            return None
-        return md_dict[job['metadata']]
-
     def grab_job_metadata(self):
         if self.current_job == None:
             return
@@ -228,27 +177,7 @@ class History:
 
         metadata = {k:v for k,v in self.gcdb.get(filename).items()
             if k != "thumbnails"}
-
-        if filename in self.metadata:
-            md_dict = self.history_ns.get([f"md_{filename}"])
-            key = -1
-            i = 0
-            for k, v in md_dict.items():
-                if int(k) > i:
-                    i = int(k)
-                if metadata['modified'] == v['modified']:
-                    key = k
-                    break
-            if key != -1:
-                self.current_job.set("metadata", key)
-            else:
-                md_dict.update({i+1: metadata})
-                self.current_job.set("metadata", i+1)
-                self.history_ns.update_child([f"md_{filename}"], md_dict)
-        else:
-            self.metadata.append(filename)
-            self.current_job.set("metadata", 0)
-            self.history_ns.insert([f"md_{filename}"], {0:metadata})
+        self.current_job.set("metadata", metadata)
 
     def save_current_job(self):
         self.history_ns.update_child(self.current_job_id,
@@ -259,7 +188,7 @@ class PrinterJob:
         self.end_time = None
         self.filament_used = 0
         self.filename = None
-        self.metadata = 0
+        self.metadata = None
         self.print_duration = 0
         self.status = "in_progress"
         self.start_time = time.time()
