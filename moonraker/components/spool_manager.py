f837e10461b7dfb7c3a9d0e081269f872d04656b
Mateusz Brzezinski <brzezi@gmail.com>
2021-03-22 14:15:27 +0100

    Address PR comments

--- moonraker/components/spool_manager.py
+++ moonraker/components/spool_manager.py
@@ -3,19 +3,22 @@
 # Copyright (C) 2021 Mateusz Brzezinski <mateusz.brzezinski@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-
+from __future__ import annotations
 import logging
 import time
 import math
-from typing import Set
-from typing import Optional
-from typing import List
-from components.database import NamespaceWrapper
-from websockets import WebRequest
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Set
+    from typing import Optional
+    from typing import List
+    from components.database import NamespaceWrapper
+    from websockets import WebRequest
 
-FILAMENT_NAMESPACE = "filament_manager"
-SPOOLS_SUB_NS = "spools"
-ACTIVE_SPOOL_KEY = "active-spool"
+
+SPOOL_NAMESPACE = "spool_manager"
+MOONRAKER_NAMESPACE = "moonraker"
+ACTIVE_SPOOL_KEY = "spool_manager.active_spool_id"
 
 MATERIALS = {
     'PLA': {'density': 1.24},
@@ -76,40 +79,34 @@ class Spool(Validation):
         return self.__dict__.copy()
 
 
-class FilamentManager:
+class SpoolManager:
     def __init__(self, config):
         self.server = config.get_server()
 
         database = self.server.lookup_component("database")
-        database.register_local_namespace(FILAMENT_NAMESPACE)
-        self.db: NamespaceWrapper = database.wrap_namespace(FILAMENT_NAMESPACE,
-                                                            parse_keys=True)
+        database.register_local_namespace(SPOOL_NAMESPACE)
+        self.db: NamespaceWrapper = database.wrap_namespace(SPOOL_NAMESPACE,
+                                                            parse_keys=False)
+        self.moonraker_db: NamespaceWrapper = database.wrap_namespace(
+            MOONRAKER_NAMESPACE, parse_keys=False)
 
-        self.endpoint = FilamentManagerEndpoint(self.server, self)
-        self.listener = FilamentManagerListener(self.server, self)
+        self.handler = SpoolManagerHandler(self.server, self)
 
-    def _spool_id(self, spool_id: str) -> str:
-        return f"{SPOOLS_SUB_NS}.{spool_id}"
-
-    def find_spool(self, spool_id: str, default = None) -> Optional[dict]:
-        return self.db.get(self._spool_id(spool_id), default)
+    def find_spool(self, spool_id: str) -> Optional[dict]:
+        return self.db.get(spool_id, None)
 
     def set_active_spool(self, spool_id: str) -> Optional[dict]:
         spool = self.find_spool(spool_id)
 
         if spool:
-            if self.db.get(ACTIVE_SPOOL_KEY):
-                self.db.update_child(ACTIVE_SPOOL_KEY, spool_id)
-            else:
-                self.db.insert(ACTIVE_SPOOL_KEY, spool_id)
-
+            self.moonraker_db[ACTIVE_SPOOL_KEY] = spool_id
             logging.info(f'Setting spool active, id: {spool_id}')
             return spool
         else:
             return None
 
     def get_active_spool_id(self) -> str:
-        return self.db.get(ACTIVE_SPOOL_KEY, None)
+        return self.moonraker_db.get(ACTIVE_SPOOL_KEY, None)
 
     def add_spool(self, data: {}) -> str:
         spool = Spool(data)
@@ -119,12 +116,12 @@ class FilamentManager:
                 f"Missing spool attributes: {missing_attrs}", 404)
 
         next_spool_id = 0
-        spools = self.db.get(SPOOLS_SUB_NS, [])
+        spools = self.db.keys()
         if spools:
-            next_spool_id = int(list(spools)[-1], 16) + 1
+            next_spool_id = int(spools[-1], 16) + 1
         spool_id = f"{next_spool_id:06X}"
 
-        self.db.insert(self._spool_id(spool_id), spool.serialize())
+        self.db[spool_id] = spool.serialize()
         logging.info(f'New spool added, id: {spool_id}')
 
         return spool_id
@@ -139,23 +136,22 @@ class FilamentManager:
             raise self.server.error(
                 f"Missing spool attributes: {missing_attrs}", 404)
 
-        self.db.update_child(self._spool_id(spool_id), spool.serialize())
+        self.db[spool_id] = spool.serialize()
         logging.info(f'Spool id: {spool_id} updated.')
 
         return
 
     def delete_spool(self, spool_id: str) -> None:
-        self.db.delete(self._spool_id(spool_id))
+        self.db.delete(spool_id)
         logging.info(f'Spool id: {spool_id} deleted.')
         return
 
     def find_all_spools(self, show_inactive: bool) -> List[dict]:
-        spools = self.db.get(SPOOLS_SUB_NS, [])
-
+        spools = self.db.items()
         if not show_inactive:
-            spools = {k: v for k, v in spools.items() if v['active'] == True}
+            spools = {k: v for k, v in spools if v['active'] is True}
 
-        return spools
+        return dict(spools)
 
     def track_filament_usage(self, job_id: str, used_length: float):
         spool_id = self.get_active_spool_id()
@@ -199,7 +195,7 @@ class FilamentManager:
             metadata = {'spool_id': spool_id,
                         'used_weight': used_weight,
                         'cost': used_cost}
-            history.add_job_metadata(job_id, {'filament': metadata})
+            history.add_job_metadata(job_id, {'spool': metadata})
 
             logging.info(f'Tracking filament usage, spool_id: {spool_id}, ' +
                          f'length: {used_length}, ' +
@@ -213,71 +209,63 @@ class FilamentManager:
             logging.info("Active spool is not set, tracking ignored")
 
 
-class FilamentManagerListener:
-    def __init__(self, server, filament_manager: FilamentManager):
-        self.filament_manager = filament_manager
+class SpoolManagerHandler:
+    def __init__(self, server, spool_manager: SpoolManager):
+        self.spool_manager = spool_manager
         self.server = server
 
         self._register_listeners()
+        self._register_endpoints()
 
     def _register_listeners(self):
         self.server.register_event_handler('history:history_changed',
                                            self._handle_history_changed)
+    def _register_endpoints(self):
+        self.server.register_endpoint(
+            "/spool_manager/spool", ['GET', 'POST', 'DELETE'],
+            self._handle_spool_request)
+        self.server.register_endpoint(
+            "/spool_manager/spool/list", ['GET'], self._handle_spools_list)
+        self.server.register_endpoint(
+            "/spool_manager/spool/active", ['GET', 'POST'],
+            self._handle_active_spool)
+        self.server.register_endpoint(
+            "/spool_manager/materials", ['GET'],
+            self._handle_materials_list)
 
-    def _handle_history_changed(self, data: {}):
+    async def _handle_history_changed(self, data: {}):
         action = data['action']
 
         if action == 'finished':
             job_data = data['job']
             job_id = job_data['job_id']
             filament_used = job_data['filament_used']
-            self.filament_manager.track_filament_usage(job_id, filament_used)
-
-
-class FilamentManagerEndpoint:
-    def __init__(self, server, filament_manager: FilamentManager):
-        self.filament_manager = filament_manager
-        self.server = server
-
-        self._register_endpoints()
-
-    def _register_endpoints(self):
-        self.server.register_endpoint(
-            "/filament/spool", ['GET', 'POST', 'DELETE'],
-            self._handle_spool_request)
-        self.server.register_endpoint(
-            "/filament/spool/list", ['GET'], self._handle_spools_list)
-        self.server.register_endpoint(
-            "/filament/active_spool", ['GET', 'POST'],
-            self._handle_active_spool)
-        self.server.register_endpoint(
-            "/filament/materials", ['GET'],
-            self._handle_materials_list)
+            self.spool_manager.track_filament_usage(job_id, filament_used)
 
     async def _handle_spool_request(self, web_request: WebRequest):
         action = web_request.get_action()
 
         if action == 'GET':
             spool_id = web_request.get_str('id')
-            spool = self.filament_manager.find_spool(spool_id)
+            spool = self.spool_manager.find_spool(spool_id)
             return {'spool': spool}
         elif action == 'POST':
             spool_id = web_request.get('id', None)
 
             if spool_id:
-                self.filament_manager.update_spool(spool_id, web_request.args)
+                self.spool_manager.update_spool(spool_id, web_request.args)
                 return 'OK'
             else:
-                spool_id = self.filament_manager.add_spool(web_request.args)
+                spool_id = self.spool_manager.add_spool(web_request.args)
                 return {'spool_added': spool_id}
         elif action == 'DELETE':
             spool_id = web_request.get_str('id')
-            self.filament_manager.delete_spool(spool_id)
+            self.spool_manager.delete_spool(spool_id)
             return 'OK'
 
     async def _handle_spools_list(self, web_request: WebRequest):
         show_inactive = web_request.get_boolean('show_inactive', False)
-        spools = self.filament_manager.find_all_spools(show_inactive)
+        spools = self.spool_manager.find_all_spools(show_inactive)
 
         return {'spools': spools}
 
@@ -285,11 +273,11 @@ class FilamentManagerEndpoint:
         action = web_request.get_action()
 
         if action == 'GET':
-            spool_id = self.filament_manager.get_active_spool_id()
+            spool_id = self.spool_manager.get_active_spool_id()
             return {"spool_id": spool_id}
         elif action == 'POST':
             spool_id = web_request.get_str('id')
-            spool = self.filament_manager.set_active_spool(spool_id)
+            spool = self.spool_manager.set_active_spool(spool_id)
             if spool:
                 return 'OK'
             else:
@@ -301,4 +289,4 @@ class FilamentManagerEndpoint:
 
 
 def load_component(config):
-    return FilamentManager(config)
+    return SpoolManager(config)
