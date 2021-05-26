261fbbc867236c2dc7ded720864961de2b1b0f96
Arksine <arksine.code@gmail.com>
2021-05-26 12:27:45 -0400

    file_manager: move modules to its own package
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

---
+++ moonraker/components/file_manager/__init__.py
@@ -0,0 +1,15 @@
+# Package definition for the file_manager
+#
+# Copyright (C) 2021 Eric Callahan <arksine.code@gmail.com>
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
+from __future__ import annotations
+from . import file_manager as fm
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from confighelper import ConfigHelper
+
+def load_component(config: ConfigHelper) -> fm.FileManager:
+    return fm.load_component(config)
--- moonraker/components/file_manager/file_manager.py
+++ moonraker/components/file_manager/file_manager.py
@@ -38,9 +38,9 @@ if TYPE_CHECKING:
     from moonraker import Server
     from confighelper import ConfigHelper
     from websockets import WebRequest
-    from . import database
-    from . import klippy_apis
-    from . import shell_command
+    from components import database
+    from components import klippy_apis
+    from components import shell_command
     DBComp = database.MoonrakerDatabase
     APIComp = klippy_apis.KlippyAPI
     SCMDComp = shell_command.ShellCommandFactory
