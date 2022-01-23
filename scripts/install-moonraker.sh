83edfc224eb46a481128bf7c865b3bac78529999
Eric Callahan <arksine.code@gmail.com>
2022-01-23 08:08:12 -0500

    scripts: add packagekit dependency to install script
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- scripts/install-moonraker.sh
+++ scripts/install-moonraker.sh
@@ -26,7 +26,7 @@ install_packages()
 {
     PKGLIST="python3-virtualenv python3-dev libopenjp2-7 python3-libgpiod"
     PKGLIST="${PKGLIST} curl libcurl4-openssl-dev libssl-dev liblmdb-dev"
-    PKGLIST="${PKGLIST} libsodium-dev zlib1g-dev libjpeg-dev"
+    PKGLIST="${PKGLIST} libsodium-dev zlib1g-dev libjpeg-dev packagekit"
 
     # Update system package info
     report_status "Running apt-get update..."
