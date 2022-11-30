dde9bcc752c4827ff0ffc01ed99c6f487122c04f
Eric Callahan <arksine.code@gmail.com>
2022-11-30 16:24:34 -0500

    metadata: add object detection logging
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- moonraker/components/file_manager/metadata.py
+++ moonraker/components/file_manager/metadata.py
@@ -138,11 +138,21 @@ class BaseSlicer(object):
                            pattern: Optional[str] = None
                            ) -> bool:
         match = re.search(
-            r"\n(DEFINE_OBJECT)|(EXCLUDE_OBJECT_DEFINE) NAME=",
+            r"\n((DEFINE_OBJECT)|(EXCLUDE_OBJECT_DEFINE)) NAME=",
             data
         )
         if match is not None:
-            # Objects alread processed
+            # Objects already processed
+            fname = os.path.basename(self.path)
+            log_to_stderr(
+                f"File '{fname}' currently supports cancellation, "
+                "processing aborted"
+            )
+            if match.group(1).startswith("DEFINE_OBJECT"):
+                log_to_stderr(
+                    "Legacy object processing detected.  This is not "
+                    "compatible with official versions of Klipper."
+                )
             return False
         # Always check M486
         patterns = [r"\nM486"]
