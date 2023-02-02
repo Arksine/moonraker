5b8dac42bb2cd9d8d82dffb3514a8c191165d47c
cmerkle <clay.merkle@gmail.com>
2023-02-02 08:32:39 -0500

    Integrated hue device group support with original hue device class, and added device_type parameter to configuration.

--- moonraker/components/power.py
+++ moonraker/components/power.py
@@ -54,8 +54,7 @@ class PrinterPower:
             "rf": RFDevice,
             "mqtt": MQTTDevice,
             "smartthings": SmartThings,
-            "hue": HueDevice,
-            "hueGroup": HueDeviceGroup
+            "hue": HueDevice
         }
 
         for section in prefix_sections:
@@ -1363,55 +1362,36 @@ class HueDevice(HTTPDevice):
     def __init__(self, config: ConfigHelper) -> None:
         super().__init__(config)
         self.device_id = config.get("device_id")
+        self.device_type = config.get("device_type","light")
+        if self.device_type == "group":
+            self.state_key = "action"
+            self.on_state = "all_on"
+        else:
+            self.state_key = "state"
+            self.on_state = "on"
+
 
     async def _send_power_request(self, state: str) -> str:
         new_state = True if state == "on" else False
         url = (
-            f"http://{self.addr}/api/{self.user}/lights/{self.device_id}/state"
+            f"http://{self.addr}/api/{self.user}/{self.device_type}s/{self.device_id}/{self.state_key}"
         )
         url = self.client.escape_url(url)
         ret = await self.client.request("PUT", url, body={"on": new_state})
         resp = cast(List[Dict[str, Dict[str, Any]]], ret.json())
         return (
-            "on" if resp[0]["success"][f"/lights/{self.device_id}/state/on"]
+            "on" if resp[0]["success"][f"/{self.device_type}s/{self.device_id}/{self.state_key}/on"]
             else "off"
         )
 
     async def _send_status_request(self) -> str:
         url = (
-            f"http://{self.addr}/api/{self.user}/lights/{self.device_id}"
+            f"http://{self.addr}/api/{self.user}/{self.device_type}s/{self.device_id}"
         )
         url = self.client.escape_url(url)
         ret = await self.client.request("GET", url)
         resp = cast(Dict[str, Dict[str, Any]], ret.json())
-        return "on" if resp["state"]["on"] else "off"
-
-class HueDeviceGroup(HTTPDevice):
-
-    def __init__(self, config: ConfigHelper) -> None:
-        super().__init__(config)
-        self.group_id = config.get("group_id")
-
-    async def _send_power_request(self, state: str) -> str:
-        new_state = True if state == "on" else False
-        url = (
-            f"http://{self.addr}/api/{self.user}/groups/{self.group_id}/action"
-        )
-        url = self.client.escape_url(url)
-        ret = await self.client.request("PUT", url, body={"on": new_state})
-        resp = cast(List[Dict[str, Dict[str, Any]]], ret.json())
-        return (
-            "on" if resp[0]["success"][f"/groups/{self.group_id}/action/on"]
-            else "off"
-        )
-    async def _send_status_request(self) -> str:
-        url = (
-            f"http://{self.addr}/api/{self.user}/groups/{self.group_id}"
-        )
-        url = self.client.escape_url(url)
-        ret = await self.client.request("GET", url)
-        resp = cast(Dict[str, Dict[str, Any]], ret.json())
-        return "on" if resp["state"]["all_on"] else "off"
+        return "on" if resp["state"][self.on_state] else "off"
 
 
 # The power component has multiple configuration sections
