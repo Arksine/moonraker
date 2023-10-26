440f76301301e8ad1f476f5c17e09b1d0b02d62d
Sandro Pischinger <mail@sandropischinger.de>
2023-10-26 23:48:07 +0200

    history: add energy_meter measurement
    If the energy_meter component is available, a measurement
    is started everytime a job is started. When the job is done,
    the measurement delta is saved to the print stats.
    
    Signed-off-by: Sandro Pischinger <mail@sandropischinger.de>

--- moonraker/components/history.py
+++ moonraker/components/history.py
@@ -22,9 +22,11 @@ if TYPE_CHECKING:
     from .database import MoonrakerDatabase as DBComp
     from .job_state import JobState
     from .file_manager.file_manager import FileManager
+    from .energy_meter import EnergyMeter, DeltaMeasurement
 
 HIST_NAMESPACE = "history"
 MAX_JOBS = 10000
+
 
 class History:
     def __init__(self, config: ConfigHelper) -> None:
@@ -33,6 +35,10 @@ class History:
             'file_manager')
         self.request_lock = Lock()
         database: DBComp = self.server.lookup_component("database")
+        self.emeter: EnergyMeter = self.server.load_component(
+            config,
+            "energy_meter",
+            default=None)
         self.job_totals: Dict[str, float] = database.get_item(
             "moonraker", "history.job_totals",
             {
@@ -247,6 +253,8 @@ class History:
         self.history_ns[job_id] = job.get_stats()
         self.cached_job_ids.append(job_id)
         self.next_job_id += 1
+        if self.emeter is not None:
+            self.emeter.start_measurement()
         logging.debug(
             f"History Job Added - Id: {job_id}, File: {job.filename}"
         )
@@ -270,6 +278,11 @@ class History:
         ):
             # Print stats have been reset, do not update this job with them
             pstats = {}
+
+        if self.emeter is not None:
+            energy_msrmt: Optional[DeltaMeasurement] = self.emeter.stop_measurement()
+            if energy_msrmt:
+                pstats.update({'energy_used': energy_msrmt.delta()})
 
         self.current_job.finish(status, pstats)
         # Regrab metadata incase metadata wasn't parsed yet due to file upload
@@ -371,6 +384,7 @@ class PrinterJob:
         self.status: str = "in_progress"
         self.start_time = time.time()
         self.total_duration: float = 0.
+        self.energy_used: Optional[Union[int, float]] = None
         self.update_from_ps(data)
 
     def finish(self,
