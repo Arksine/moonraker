ccfe32f2368a5ff6c2497478319909daeeeb8edf
Eric Callahan <arksine.code@gmail.com>
2024-11-17 15:18:32 -0500

    datastore: store multiline commands as one entry
    Signed-off-by:  Eric Callahan <arksine.code@gmail.com>

--- moonraker/components/data_store.py
+++ moonraker/components/data_store.py
@@ -155,12 +155,9 @@ class DataStore:
 
     def _store_gcode_command(self, script: str) -> None:
         curtime = time.time()
-        for cmd in script.split('\n'):
-            cmd = cmd.strip()
-            if not cmd:
-                continue
+        if script.strip():
             self.gcode_queue.append(
-                {'message': cmd, 'time': curtime, 'type': "command"}
+                {'message': script, 'time': curtime, 'type': "command"}
             )
 
     async def _handle_gcode_store_request(self,
